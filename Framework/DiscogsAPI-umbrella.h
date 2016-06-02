1f634964de7a82ea8cfcfe827df40e71d323c4f9
Maxime Epain <maxime.epain@gmail.com>
2016-06-02 07:46:45 +0200

    Add Framework project

---
+++ Framework/DiscogsAPI-umbrella.h
@@ -0,0 +1,22 @@
+//
+//  DiscogsAPI.h
+//  DiscogsAPI
+//
+//  Created by Maxime Epain on 01/06/16.
+//  Copyright © 2016 Maxime Epain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiscogsAPI.
+FOUNDATION_EXPORT double DiscogsAPIVersionNumber;
+
+//! Project version string for DiscogsAPI.
+FOUNDATION_EXPORT const unsigned char DiscogsAPIVersionString[];
+
+#import <DiscogsAPI/DiscogsAPI.h>
+#import <DiscogsAPI/DGAuthentication.h>
+#import <DiscogsAPI/DGDatabase.h>
+#import <DiscogsAPI/DGUser.h>
+#import <DiscogsAPI/DGResource.h>
+#import <DiscogsAPI/DGMarketplace.h>
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target "DiscogsAPI" do
+	pod 'AFOAuth1Client', '~> 1.0.0'
+    pod 'RestKit/ObjectMapping', '~> 0.26.0'
+    pod 'RestKit/Network', '~> 0.26.0'
+end
